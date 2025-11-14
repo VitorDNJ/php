b1b5999d2457bd8872512630182afe006f47ce13
VitorDNJ <vitorjustino980@gmail.com>
2025-11-13 22:43:27 -0300

    criando uma função para verificar se o número é impa ou par

---
+++ 10 exercícios/exercício04.php
@@ -0,0 +1,17 @@
+<?php 
+// Exercício 04: verificando se o número é par ou ímpar
+
+function verificarPArouImpa($n){
+if($n %2 ==0){  
+return "O número $n é PAR";
+}else{
+return"O número $n é IMPAR";
+}
+}
+
+ $numero = 15;
+ $resultado = verificarPArouImpa($numero);
+
+ print $resultado;
+
+?>
